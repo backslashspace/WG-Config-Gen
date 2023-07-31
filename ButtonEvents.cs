fcf0bcc7f187f19e2da6e0f827f3d9faf664218e
backslashspace <tmbt.net@gmail.com>
2023-07-31 22:11:22 +0200

    Initial Commit

---
+++ ButtonEvents.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace wg_confGen
+{
+    public partial class MainWindow
+    {
+        //nav buttons
+        private static Byte PageAmmount = 3;
+        private static Byte CurrentPage = 0;
+
+        private void NextPage(object sender, RoutedEventArgs e)
+        {
+            if (CurrentPage == PageAmmount)
+            {
+                return;
+            }
+
+            BackButton.IsEnabled = true;
+
+            ChangePageView(CurrentPage, ++CurrentPage);
+
+            if (CurrentPage == PageAmmount)
+            {
+                NxtButton.IsEnabled = false;
+            }
+        }
+
+        private void PageBack(object sender, RoutedEventArgs e)
+        {
+            if (CurrentPage == 0)
+            {
+                return;
+            }
+
+            NxtButton.IsEnabled = true;
+
+            ChangePageView(CurrentPage, --CurrentPage);
+
+            if (CurrentPage == 0)
+            {
+                BackButton.IsEnabled = false;
+            }
+        }
+
+        private void ChangePageView(Byte OldPage, Byte NewPage)
+        {
+            if (CreateServerBox.IsChecked == false) 
+            {
+            
+            
+            
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //show new
+            switch (NewPage)
+            {
+                case 0:
+                    StartGrid.Visibility = Visibility.Visible;
+                    break;
+
+                case 1:
+                    Page1.Visibility = Visibility.Visible;
+                    break;
+
+                case 2:
+                    Page2.Visibility = Visibility.Visible;
+                    break;
+
+                case 3:
+                    Page3.Visibility = Visibility.Visible;
+                    break;
+            }
+
+            //deactivate old
+            switch (OldPage)
+            {
+                case 0:
+                    StartGrid.Visibility = Visibility.Collapsed;
+                    break;
+
+                case 1:
+                    Page1.Visibility = Visibility.Collapsed;
+                    break;
+
+                case 2:
+                    Page2.Visibility = Visibility.Visible;
+                    break;
+
+                case 3:
+                    Page3.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        //# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+        //main page
+        private void CreateServerConfig(object sender, RoutedEventArgs e)
+        {
+            if ((bool)CreateServerBox.IsChecked)
+            {
+                S2S.IsEnabled = true;
+            }
+            else
+            {
+                S2S.IsEnabled = false;
+                S2S.IsChecked = false;
+            }
+        }
+
+        private void ClientBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (UInt32.TryParse(ClientBox.Text, out Clients))
+            {
+                ClientBox.Foreground = Brushes.Black;
+
+                SetButtonStatus(true);
+            }
+            else
+            {
+                SetButtonStatus(false);
+
+                ClientBox.Foreground = Brushes.OrangeRed;
+            }
+
+            void SetButtonStatus(Boolean NewState)
+            {
+                Dispatcher.BeginInvoke(new Action(() => { NxtButton.IsEnabled = NewState; }), System.Windows.Threading.DispatcherPriority.DataBind, null);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Reflection;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections;
+using System.IO;
+using System.Windows.Forms;
+
+namespace wg_confGen
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string wg_exe = "C:\\Program Files\\WireGuard\\wg.exe";
+
+        public MainWindow()
+        {
+            if (!File.Exists(wg_exe))
+            {
+                System.Windows.Forms.MessageBox.Show(
+                "wgcg",
+                "wg.exe not found, Wireguard not installed?\n\nPath = \"" + wg_exe + "\"",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+
+                Environment.Exit(1);
+            }
+
+            InitializeComponent();
+        }
+
+        //# # # # # # # # # # # # # # # # #
+
+        private static UInt32 Clients = 0;
+
+        //# # # # # # # # # # # # # # # # #
+
+        private void Debug(object sender, RoutedEventArgs e)
+        {
+
+            
+
+        }
+
+        //# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+        ///<summary>Launches executables.</summary>
+        ///<exception cref="FileLoadException"></exception>
+        private static (String, Int32) EXE(String Path, String Args = null, Boolean RunAs = false, Boolean InternalOutputRedirect = false, Boolean HiddenExecute = false, Boolean WaitForExit = false, String WorkingDirectory = null, Boolean RedirectErrors = false)
+        {
+            Process process = new();
+
+            process.StartInfo.FileName = Path;
+
+            if (RunAs)
+            {
+                process.StartInfo.Verb = "runas";
+            }
+
+            if (HiddenExecute)
+            {
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.StartInfo.CreateNoWindow = true;
+            }
+
+            if (InternalOutputRedirect)
+            {
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+
+                if (RedirectErrors)
+                {
+                    process.StartInfo.RedirectStandardError = true;
+                }
+                else
+                {
+                    process.StartInfo.RedirectStandardError = false;
+                }
+            }
+            else
+            {
+                process.StartInfo.RedirectStandardOutput = false;
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.RedirectStandardError = false;
+            }
+
+            if (WorkingDirectory != null)
+            {
+                process.StartInfo.WorkingDirectory = WorkingDirectory;
+            }
+
+            if (Args != null)
+            {
+                process.StartInfo.Arguments = Args;
+            }
+
+            process.Start();
+
+            if (InternalOutputRedirect)
+            {
+                String Output = process.StandardOutput.ReadToEnd();
+
+                process.WaitForExit();
+
+                if (RedirectErrors)
+                {
+                    Output += process.StandardError.ReadToEnd();
+                }
+
+                return (Output, process.ExitCode);
+            }
+            else if (WaitForExit)
+            {
+                process.WaitForExit();
+
+                return (null, process.ExitCode);
+            }
+
+            return (null, Int32.MaxValue - 1);
+        }
+    }
+}
